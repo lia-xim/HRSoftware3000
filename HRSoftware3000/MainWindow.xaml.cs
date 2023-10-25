e4d746ddca434094b93c000900ff9ae3dab2979f
Liaxim <matthiasramahi@web.de>
2023-10-25 19:40:30 +0200

    Initial commit

---
+++ HRSoftware3000/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HRSoftware3000
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void goToDashboard(object sender, RoutedEventArgs e)
+        {
+            mainWindowFrame.Source = new Uri("Pages/dashboard.xaml", UriKind.Relative);
+        }
+
+        private void goToEmployees(object sender, RoutedEventArgs e)
+        {
+            mainWindowFrame.Source = new Uri("Pages/employees.xaml", UriKind.Relative);
+        }
+
+        private void goToProjects(object sender, RoutedEventArgs e)
+        {
+            mainWindowFrame.Source = new Uri("Pages/projects.xaml", UriKind.Relative);
+        }
+    }
+}
