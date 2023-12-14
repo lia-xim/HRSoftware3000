d355abd78722fcfcabae8722c02f1c79443270d8
Nikolas <nikolas.behm@web.de>
2023-12-15 00:02:49 +0100

    Added: db and query

---
+++ HRSoftware3000/AddressBookDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace HRSoftware3000
+{
+
+
+    partial class AddressBookDataSet
+    {
+        partial class tblContactsDataTable
+        {
+        }
+    }
+}
+
+namespace HRSoftware3000.AddressBookDataSetTableAdapters {
+    
+    
+    public partial class tblContactsTableAdapter {
+    }
+}
--- HRSoftware3000/Pages/employees.xaml.cs
+++ HRSoftware3000/Pages/employees.xaml.cs
@@ -23,6 +23,34 @@ namespace HRSoftware3000.Pages
         public employees()
         {
             InitializeComponent();
+            string verbindungsstring = @"Provider=Microsoft.ACE.OLEDB.12.0;
+                                       Data Source=C:\Users\Nikolas\source\repos\HRSoftware3000\HRSoftware3000\Projekte1 neu.accdb";
+            System.Data.OleDb.OleDbConnection dBVerbindung = null;
+            System.Data.OleDb.OleDbCommand befehl = null;
+            System.Data.OleDb.OleDbDataReader datenleser = null;
+            bool offen = false;
+            try
+            {
+                dBVerbindung = new System.Data.OleDb.OleDbConnection(verbindungsstring);
+                dBVerbindung.Open();
+                offen = true;
+                befehl = dBVerbindung.CreateCommand();
+                befehl.CommandText = "SELECT * FROM Mitarbeiter";
+                datenleser = befehl.ExecuteReader();
+                while (datenleser.Read())
+                {
+                    MessageBox.Show(datenleser.GetString(1));
+                    MessageBox.Show("Erste Spalte: " + datenleser.GetInt32(0));
+                }
+            }
+            catch (Exception ausnahme)
+            {
+                MessageBox.Show("Datenbankfehler: " + ausnahme.Message);
+            }
+            finally
+            {
+                if (offen == true) dBVerbindung.Close();
+            }
         }
     }
 }
