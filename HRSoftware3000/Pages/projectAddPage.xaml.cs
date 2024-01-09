2edb4893edef016833dd61c14151bf4d4eed4922
Nikolas <nikolas.behm@web.de>
2024-01-09 15:14:26 +0100

    Added: add project

--- HRSoftware3000/Pages/projectAddPage.xaml.cs
+++ HRSoftware3000/Pages/projectAddPage.xaml.cs
@@ -20,9 +20,53 @@ namespace HRSoftware3000.Pages
     /// </summary>
     public partial class projectAddPage : Window
     {
-        public projectAddPage()
-        {
-            InitializeComponent();
+            string verbindungsstring = @"Provider=Microsoft.ACE.OLEDB.12.0;
+                                       Data Source=./Projekte1 neu.accdb";
+            System.Data.OleDb.OleDbConnection dBVerbindung = null;
+            System.Data.OleDb.OleDbCommand befehl = null;
+            bool offen = false;
+            public projectAddPage()
+            {
+                InitializeComponent();
+
+                Button saveButton = new Button();
+                saveButton.Name = "addProjectBtn";
+                saveButton.Click += addProject_Click;
+
+            }
+            private void addProject_Click(object sender, RoutedEventArgs e)
+            {
+                try
+                {
+                    dBVerbindung = new System.Data.OleDb.OleDbConnection(verbindungsstring);
+                    dBVerbindung.Open();
+                    offen = true;
+                    befehl = dBVerbindung.CreateCommand();
+                    befehl.CommandText = "INSERT INTO Projekte ([id],[projektbezeichnung],[startdatum],[enddatum],[startphase], [verantwortlicher]) VALUES(?, ?, ?, ?, ?,? ); ";
+                    int id = 26;
+                    string nachn = "Nachname";
+                    string vorn = "Vorname";
+                    string abteilung = "Abteilung";
+                    string telefon = "5555";
+                    befehl.Parameters.AddWithValue("id", Id_Text.Text);
+                    befehl.Parameters.AddWithValue("projektbezeichnung", Bezeichnung_Text.Text);
+                    befehl.Parameters.AddWithValue("startdatum", DateTime.Parse(Startdatum_Text.Text));
+                    befehl.Parameters.AddWithValue("enddatum", DateTime.Parse(Enddatum_Text.Text));
+                    befehl.Parameters.AddWithValue("startphase", Startphase_Text.Text);
+                    befehl.Parameters.AddWithValue("verantwortlicher", Verantwortlicher_Text.Text);
+
+                    befehl.ExecuteNonQuery();
+                    MessageBox.Show("Projekt erfolgreich eingefügt." + Environment.NewLine + "Projekte neu laden um Änderung zu sehen");
+                }
+                catch (Exception ausnahme)
+                {
+                    MessageBox.Show("Datenbankfehler: " + ausnahme.Message);
+                }
+                finally
+                {
+                    if (offen == true) dBVerbindung.Close();
+                }
+            }
         }
-    }
+    
 }
